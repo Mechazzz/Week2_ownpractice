0caeeabb286112252b5c1974974ec0046379946d
Mechazzz <129221431+Mechazzz@users.noreply.github.com>
2023-09-25 14:12:49 +0200

    update 2023.09.25

--- script.js
+++ script.js
@@ -1,108 +1,34 @@
-let practice = [ {
-    name: "Opel",
-    color: "white",
-    speed: "mid",
-    expensive: "no",
-} , {
-    name: "Volvo",
-    color: "dark",
-    speed:"mid",
-    expensive: "no",
-} , {
-    name: "BMW",
-    color: "red",
-    speed: "high",
-    expensive: "yes",
+let rootElement = document.querySelector('#root');
+
+let htmlContent = "<h2> The text </h2>"
+
+rootElement.insertAdjacentHTML('beforebegin', htmlContent)
+
+/* let counter= 0;
+while (counter < 10 ) {
+  rootElement.insertAdjacentHTML('beforebegin', htmlContent)
+  counter++ ;
+} */
+
+let extraContent = "<h3> The other text </h3>"
+
+rootElement.insertAdjacentHTML('beforeend', extraContent)
+
+/* for (let counter = 0; counter <= 5 ; counter++) {
+  console.log(counter);
+} */
+
+/* for (let counter = 0; counter <= 10; counter++) {
+  rootElement.insertAdjacentHTML('beforebegin', htmlContent)
+} */
+
+/* let array = ["alma", "körte", "szilva", "eper", "birs", "vegyes"]
+for (let counter= 0; counter <= 10; counter++) {
+  rootElement.insertAdjacentHTML('beforebegin', `<h2> ${array[3]} </h2>`)
+} */
+
+let array = ["alma", "körte", "szilva", "eper", "birs", "vegyes"]
+for (let index= 0; index < array.length; index++) {
+  rootElement.insertAdjacentHTML('beforebegin', `<h2> ${array[0]} </h2>`)
 }
-]
 
-console.log(practice)
-console.log(practice[2])
-
-
-let myFacebook = {
-    name: "Bakó István",
-    age: 29,
-    location: "Budapest",
-    friends: ["Gipsz Jakab", "John Doe", "Példa Péter"],
-    favouriteFilms: [
-      {
-        title: "Harry Potter",
-        year: 2001,
-        characters: ["Harry Potter", "Ronald Weasley", "Hedvig"],
-      },
-      {
-        title: "Ananász Express",
-        year: 2012,
-        characters: ["Seth Rogen", "Péter"],
-      },
-      {
-        title: "Üvegtigris",
-        year: 2004,
-        characters: ["Józsi", "Béla"],
-      },
-    ],
-  };
-
-  console.log(myFacebook.favouriteFilms[2].characters[1])
-  console.log(myFacebook.favouriteFilms[0].title)
-
-
-  let myObj = {
-    key1: "value1",
-    key2: "value2",
-    key3: "value3",
-    key4: 15,
-    key5: 600,
-    key6: true,
-    key7: ["Imola Ács", "Pali Herrer", "Nagy Zsombor", "Ágota Bokor"],
-  };
-
-  console.log(myObj.key7[1])
-  console.log(myObj.key1)
-
-  let myArr = [
-    ["Alma", "Körte", "Szilva"],
-    ["Dreher", "Kőbányai", "Kanizsai sör"],
-    ["Mizse", "Szentkirályi", "Tesco gazd"],
-  ];
-
-  console.log(myArr[1])
-  console.log(myArr[2])
-  console.log(myArr[1][2])
-  console.log(myArr)
-
-// Hazi : 
-
-  let object = [ "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"]
-  console.log(object[3])
-
-  myNewObject = {
-    key1: "one",
-    key2:"two",
-    key3: ["Drink", "Food", "Car", "House", "Cat"],
-    key4: "four",
-    key5: "five",
-    key6: "six",
-    key7: "seven",
-    key8: "eight",
-    key9: "nine",
-    key10: [ {
-        type: "Not the best",
-        good: "Not quite",
-        cheap: "Yes, very cheap" ,
-    } ,
-    {
-        type:"Amazing",
-        good: [["Yes, of course"], ["We can say so", "All right", "The Best!"], ["You will not believe !"]],
-        cheap: "Unfortunately not",
-    }
-    ]
-  }
-
-  console.log(myNewObject.key2)
-  console.log(myNewObject.key3[2])
-  console.log(myNewObject.key10[1].cheap)
-  console.log(myNewObject.key10[1].good[2])
-  console.log(myNewObject.key10[1].good[1][2])
-  
