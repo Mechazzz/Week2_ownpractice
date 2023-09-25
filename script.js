0b8df808a41305b2f6bfc3b5adbeff4fc5102360
Mechazzz <129221431+Mechazzz@users.noreply.github.com>
2023-09-25 15:31:40 +0200

    update 2 2023.09.25

--- script.js
+++ script.js
@@ -1,34 +1,5 @@
-let rootElement = document.querySelector('#root');
+let rootElement = document.querySelector('#root')
 
-let htmlContent = "<h2> The text </h2>"
 
-rootElement.insertAdjacentHTML('beforebegin', htmlContent)
 
-/* let counter= 0;
-while (counter < 10 ) {
-  rootElement.insertAdjacentHTML('beforebegin', htmlContent)
-  counter++ ;
-} */
-
-let extraContent = "<h3> The other text </h3>"
-
-rootElement.insertAdjacentHTML('beforeend', extraContent)
-
-/* for (let counter = 0; counter <= 5 ; counter++) {
-  console.log(counter);
-} */
-
-/* for (let counter = 0; counter <= 10; counter++) {
-  rootElement.insertAdjacentHTML('beforebegin', htmlContent)
-} */
-
-/* let array = ["alma", "körte", "szilva", "eper", "birs", "vegyes"]
-for (let counter= 0; counter <= 10; counter++) {
-  rootElement.insertAdjacentHTML('beforebegin', `<h2> ${array[3]} </h2>`)
-} */
-
-let array = ["alma", "körte", "szilva", "eper", "birs", "vegyes"]
-for (let index= 0; index < array.length; index++) {
-  rootElement.insertAdjacentHTML('beforebegin', `<h2> ${array[0]} </h2>`)
-}
 
